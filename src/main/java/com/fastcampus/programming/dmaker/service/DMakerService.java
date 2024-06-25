eafc1a2b0eebe5d64188ea57b109722f0d945308
luvi-kh <nalsengom@daum.net>
2024-06-25 20:40:51 +0900

    AOP가 필요한 이유에서 transaction의 긴 코드 예시

--- src/main/java/com/fastcampus/programming/dmaker/service/DMakerService.java
+++ src/main/java/com/fastcampus/programming/dmaker/service/DMakerService.java
@@ -4,6 +4,8 @@ import com.fastcampus.programming.dmaker.entity.Developer;
 import com.fastcampus.programming.dmaker.repository.DeveloperRepository;
 import com.fastcampus.programming.dmaker.type.DeveloperLevel;
 import com.fastcampus.programming.dmaker.type.DeveloperSkillType;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,17 +14,39 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class DMakerService {
     private  final DeveloperRepository developerRepository;
+    private final EntityManager em;
 
+    // ACID
+    // Atomic
+    // Consistency
+    // Isolation
+    // Durability
     @Transactional
     public  void createDeveloper() {
-        Developer developer = Developer.builder()
-                .developerLevel(DeveloperLevel.JUNIOR)
-                .developerSkillType(DeveloperSkillType.FRONT_END)
-                .experienceYears(2)
-                .name("Olaf")
-                .age(5)
-                .build();
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
 
-        developerRepository.save(developer);
+            //business logic start
+            Developer developer = Developer.builder()
+                    .developerLevel(DeveloperLevel.JUNIOR)
+                    .developerSkillType(DeveloperSkillType.FRONT_END)
+                    .experienceYears(2)
+                    .name("Olaf")
+                    .age(5)
+                    .build();
+
+            /* A -> B 1만원 송금 */
+            // A 계좌에서 1만원 줄임
+            developerRepository.save(developer);
+            // B 계좌에서 1만원 늘림
+            developerRepository.delete(developer1);
+            // business logic end
+
+            transaction.commit();
+        }catch (Exception e) {
+            transaction.rollback();
+            throw  e;
+        }
     }
 }
