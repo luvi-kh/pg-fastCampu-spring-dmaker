f3e52b564304031ecf726dc7f09c4d8403cffba9
luvi-kh <nalsengom@daum.net>
2024-06-26 01:59:54 +0900

    6.비즈니스 밸리데이션 적용

--- src/main/java/com/fastcampus/programming/dmaker/service/DMakerService.java
+++ src/main/java/com/fastcampus/programming/dmaker/service/DMakerService.java
@@ -2,31 +2,60 @@ package com.fastcampus.programming.dmaker.service;
 
 import com.fastcampus.programming.dmaker.dto.CreateDeveloper;
 import com.fastcampus.programming.dmaker.entity.Developer;
+import com.fastcampus.programming.dmaker.exception.DMakerException;
 import com.fastcampus.programming.dmaker.repository.DeveloperRepository;
 import com.fastcampus.programming.dmaker.type.DeveloperLevel;
 import com.fastcampus.programming.dmaker.type.DeveloperSkillType;
 import jakarta.transaction.Transactional;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static com.fastcampus.programming.dmaker.exception.DMakerErrorCode.DUPLICATED_MEMBER_ID;
+import static com.fastcampus.programming.dmaker.exception.DMakerErrorCode.LEVEL_EXPERIENCE_YEARS_NOT_MATCHED;
 
 @Service
 @RequiredArgsConstructor
 public class DMakerService {
-    private  final DeveloperRepository developerRepository;
+    private final DeveloperRepository developerRepository;
 
     @Transactional
-    public  void createDeveloper(CreateDeveloper.Request request) {
+    public void createDeveloper(CreateDeveloper.Request request) {
 
-            //business logic start
-            Developer developer = Developer.builder()
-                    .developerLevel(DeveloperLevel.JUNIOR)
-                    .developerSkillType(DeveloperSkillType.FRONT_END)
-                    .experienceYears(2)
-                    .name("Olaf")
-                    .age(5)
-                    .build();
+        validateCreateDeveloperRequest(request);
 
-            developerRepository.save(developer);
+        //business logic start
+        Developer developer = Developer.builder()
+                .developerLevel(DeveloperLevel.JUNIOR)
+                .developerSkillType(DeveloperSkillType.FRONT_END)
+                .experienceYears(2)
+                .name("Olaf")
+                .age(5)
+                .build();
+
+        developerRepository.save(developer);
+    }
+
+    private void validateCreateDeveloperRequest(CreateDeveloper.Request request) {
+        // business validation
+        DeveloperLevel developerLevel = request.getDeveloperLevel();
+        Integer experienceYears = request.getExperienceYears();
+        if (developerLevel == DeveloperLevel.SENIOR
+                && experienceYears < 10) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+        if (developerLevel == DeveloperLevel.JUNGNIOR
+                && (experienceYears < 4 || experienceYears > 10)) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+        if (developerLevel == DeveloperLevel.JUNIOR && experienceYears > 4) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+
+        developerRepository.findByMemberId(request.getMemberId())
+                .ifPresent(developer -> {
+                    throw new DMakerException(DUPLICATED_MEMBER_ID)
+                });
     }
 }
